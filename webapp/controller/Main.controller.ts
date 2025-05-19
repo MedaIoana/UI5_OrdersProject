2d8498ad45a53ef5b19b833f9e138f62095f26bc
MedaIoana <meda.ioanamaria@yahoo.com>
2025-05-19 17:46:38 +0300

    feat: Main view and features to select and delete

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -6,7 +6,6 @@ import ODataModel from "sap/ui/model/odata/v2/ODataModel";
  * @namespace ui5training.controller
  */
 export default class Main extends BaseController {
-
 	public onInit(): void {
 		console.log("Main controller initialized");
 	}
